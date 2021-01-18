1ee7b81433bc8fe9316943548de4a731d66ef6dd
DakCreed <dashnak96@gmail.com>
2021-01-18 12:36:08 +0300

    Lab 2 informatica is DONE

--- Информатика/лр/Проект-4-2-Лаб/Проект-4-2/Проект-4-2/Form1.Designer.vb
+++ Информатика/лр/Проект-4-2-Лаб/Проект-4-2/Проект-4-2/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Форма переопределяет dispose для очистки списка компонентов.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,99 +20,100 @@ Partial Class Form1
     'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
     'Для ее изменения используйте конструктор форм Windows Form.  
     'Не изменяйте ее в редакторе исходного кода.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(21, 20)
+        Me.Label1.Location = New System.Drawing.Point(50, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(86, 17)
+        Me.Label1.Size = New System.Drawing.Size(295, 13)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Исходный X"
+        Me.Label1.Text = "Решить задачу вычисления арифметических выражений"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(270, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Вычислить"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(160, 20)
+        Me.Label2.Location = New System.Drawing.Point(35, 107)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(86, 17)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Исходный Y"
+        Me.Label2.Size = New System.Drawing.Size(81, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Переменная X"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(24, 51)
+        Me.TextBox1.Location = New System.Drawing.Point(122, 104)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 2
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(163, 51)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox2.TabIndex = 3
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(24, 93)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(119, 40)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Вычислить"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(163, 93)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(119, 41)
-        Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Завершить"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'ListBox1
-        '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(24, 196)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(176, 196)
-        Me.ListBox1.TabIndex = 6
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "5,305"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(247, 196)
+        Me.Label3.Location = New System.Drawing.Point(35, 251)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(111, 17)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Пример данных"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Результат"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(122, 251)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(122, 174)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        Me.TextBox3.Text = "-0,084"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(35, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(81, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Переменная Y"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -121,11 +122,11 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents ListBox1 As ListBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
 End Class
--- Информатика/лр/Проект-4-2-Лаб/Проект-4-2/Проект-4-2/Form1.vb
+++ Информатика/лр/Проект-4-2-Лаб/Проект-4-2/Проект-4-2/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports System.Math
+Public Class Form1
+
     Dim Birds As Short
     Dim Insects As Integer
     Dim WorldPop As Long
@@ -10,89 +12,33 @@
     Dim Dog As String
     Dim Flag As Boolean
     Dim Birthday As Date
+    Dim LName = "Иванов"
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        End
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim peremX As Double
+        Dim peremY As Double
+        Dim result As Double
+        peremX = TextBox1.Text
+        peremY = TextBox3.Text
+        result = -((2 * peremX ^ 2) - Sin(peremX ^ 2) / (2 - Exp(-peremY)))
+        TextBox2.Text = result
+
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim LName As String
-        LName = "Дрожжин"
-        Label1.Text = LName
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
 
     End Sub
 
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
-        Select Case ListBox1.SelectedIndex
-            Case 0
-                Birds = 12500
-                Label3.Text = Birds
-            Case 1
-                Insects = 37500000
-                Label3.Text = Insects
-            Case 2
-                WorldPop = 4800000004
-                Label3.Text = WorldPop
-            Case 3
-                Price = 899.99
-                Label3.Text = Price
-            Case 4
-                Pi = 3.1415926535
-                Label3.Text = Pi
-            Case 5
-                Debt = 7600300.5
-                Label3.Text = Debt
-            Case 6
-                RetKey = 13
-                Label3.Text = RetKey
-            Case 7
-                UnicodeChar = "Д"
-                Label3.Text = UnicodeChar
-            Case 8
-                Dog = "pointer"
-                Label3.Text = Dog
-            Case 9
-                Flag = True
-                Label3.Text = Flag
-            Case 10
-                Birthday = #3/1/1963#
-                Label3.Text = Birthday
-            Case 8
-                Dog = "pointer"
-                Label3.Text = Dog
-            Case 9
-                Flag = True
-                Label3.Text = Flag
-            Case 10
-                Birthday = #3/1/1963#
-                Label3.Text = Birthday
-        End Select
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ListBox1.Items.Add("Short")
-        ListBox1.Items.Add("Integer")
-        ListBox1.Items.Add("Long")
-        ListBox1.Items.Add("Single")
-        ListBox1.Items.Add("Double")
-        ListBox1.Items.Add("Decimal")
-        ListBox1.Items.Add("Byte")
-        ListBox1.Items.Add("Char")
-        ListBox1.Items.Add("String")
-        ListBox1.Items.Add("Boolean")
-        ListBox1.Items.Add("Date")
-
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
 
     End Sub
 End Class
